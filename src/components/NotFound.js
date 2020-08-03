4b40c7c362ee08aa8b2a49a7d6daf99c9368513c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-03 17:15:54 -0300

    Add not-found icon

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="main-content not-found">
+    <img src="/alert.svg" id="not-found" className="not-found"></img>
+    <h2>Page Not Found</h2>
+  </div>
+);
+
+export default NotFound;
