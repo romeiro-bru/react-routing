f40ebe5ffeedfc730d38d884652e316276777c3f
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-03 13:58:32 -0300

    Nested routes - courses

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import HTML from './courses/HTML';
+import Css from './courses/Css';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
+    <Route path="/courses/html" component={HTML} />
+    <Route path="/courses/css" component={Css} />
+    <Route path="/courses/javascript" component={JavaScript}  />
   </div>
 );
 
