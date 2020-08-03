bae9add490f3a39557295697e59d9a81b64e5750
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-03 17:14:53 -0300

    Add Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={ Home } />
-      <Route path="/about"render={ () => <About title="About"/> } />
-      <Route path="/teachers" component={ Teachers } />
-      <Route path="/courses" component={ Courses } />
+      {/* wrapp our Routes in the Switch component */}
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/about"render={ () => <About title="About"/> } />
+        <Route path="/teachers" component={ Teachers } />
+        <Route path="/courses" component={ Courses } />
+        {/* ROUTE TO ERROR PAGE, we do not specify a path */}
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
