9345de25f653ced2f4cdcb29c8fa7b8d1dd4cc2d
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-13 15:19:26 -0300

    Updates

---
+++ precache-manifest.e8b27c3992778e4a2796e987fc1fd6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911d33ae7a2d915e801d353d5650e952",
+    "url": "/react-routing/index.html"
+  },
+  {
+    "revision": "a0d292595b9552f8759a",
+    "url": "/react-routing/static/css/main.16888ed8.chunk.css"
+  },
+  {
+    "revision": "1d0043ff6609495a4d44",
+    "url": "/react-routing/static/js/2.e28743c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-routing/static/js/2.e28743c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d292595b9552f8759a",
+    "url": "/react-routing/static/js/main.8ae289a1.chunk.js"
+  },
+  {
+    "revision": "35a0637b279759d7ba72",
+    "url": "/react-routing/static/js/runtime-main.1a587410.js"
+  }
+]);
